4f6fc5264c32009371c86538a49aa313ebdd9372
jeongdawun <jeongdawun33@gmail.com>
2023-04-16 14:41:30 +0900

    feat: 자바 공부 저장소 설정 완료 및 예제 1번 풀이

---
+++ Java/example1/src/example1/Pro1.java
@@ -0,0 +1,14 @@
+package example1;
+
+// 정수형 변수 1개, float 변수 2개, double 변수 3개에 아무 값이나 할당하고 출력합니다.
+// [출처] 문제 은행 [ 1 ] (에디로봇아카데미) | 작성자 링크쌤
+public class Pro1 {
+    public static void main(String[] args) {
+        final int num1 = 3;
+        final float num2 = 5.123456789f;
+        final double num3 = 4.123456789123456789;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
